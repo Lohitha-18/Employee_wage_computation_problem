cdd3c6c12ee085001661b4062f09d017b6cfed6a
lohitha <devarajlohitha@gmail.com>
2023-03-21 19:03:05 +0530

    Added:store day and daily wage along totalwage

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,61 +1,65 @@
-
-
 #!/bin/bash
 
-EMPLOYEE_WAGE_PER_HOUR=20;
-WORKING_HOUR=8;
 PRESENT=1;
 PART_TIME=2;
+WAGE_PER_HOUR=20;
+WORKING_HOUR=8;
 MAX_WORKING_HOUR=40;
-MAX_WORKING_DAYS=20;
-totalWorkingHour=0;
-day=0;
+MAX_WORKING_DAY=20;
 
-function calculateWorkingHour() {
+declare -A dailyWage;
+
+function getWorkingHour() {
+
 	case $1 in
+
 		$PRESENT)
-			 workingHour=$WORKING_HOUR;
+			workingHour=$WORKING_HOUR;
 		;;
+
 		$PART_TIME)
 			workingHour=$((WORKING_HOUR/2));
 		;;
+
 		*)
 			workingHour=0;
 		;;
+
 	esac
+
 	echo $workingHour;
 }
 
+totalWorkingHour=0;
 
-while [[ $day -le MAX_WORKING_HOUR && $totalWorkingHour -lt MAX_WORKING_HOUR ]]
+day=0;
+
+while [[ $day -lt $MAX_WORKING_DAY && $totalWorkingHour -lt $MAX_WORKING_HOUR ]]
 do
-	if [ $totalWorkingHour -eq $((MAX_WORKING_HOUR-WORKING_HOUR/2)) ]
+	if [ $totalWorkingHour -eq $((MAX_WORKING_HOUR - WORKING_HOUR/2)) ]
 	then
 		isPresent=$PART_TIME;
 	else
 		isPresent=$((RANDOM%3));
 	fi
-	
-	empHour=$(calculateWorkingHour $isPresent);
-	dailyWage[((day++))]=$((empHour * EMPLOYEE_WAGE_PER_HOUR));
 
-	totalWorkingHour=$((totalWorkingHour + empHour));
+	empHr=$(getWorkingHour $isPresent);
+	dailyWage["Day"$((day+1))]=$(( empHr * WAGE_PER_HOUR ));
+	totalWorkingHour=$((totalWorkingHour + empHr));
+	((day++));
 done
 
-totalSalary=$(($totalWorkingHour * EMPLOYEE_WAGE_PER_HOUR))
+totalSalary=$((WAGE_PER_HOUR * totalWorkingHour));
+echo "Total salary of month : $"$totalSalary "USD";
+echo "Total Working day : $day";
 
-echo "Employee Total Working Hour: " $totalWorkingHour;
-echo "Monthly Wage : " $totalSalary;
-echo "Total Working Days : " $day;
+echo "Daily_Wage : " ${dailyWage[@]};
+echo "Keys : " ${!dailyWage[@]};
+echo "Size : " ${#dailyWage[@]};
 
-echo "-------------------------------------------------";
+echo "----------------------------------------------";
 
-echo "DailyWage : " ${dailyWage[@]};
-echo "Size of DailyWage Array : "${#dailyWage[@]};
-
-echo "-------------------------------------------------";
-
-for((i=0;i<${#dailyWage[@]};i++))
+for ((i=0;i<${#dailyWage[@]};i++))
 do
-	echo "Day $i earning : " ${dailyWage[i]} "USD";
+	echo "Day$((i+1)) : $"${dailyWage["Day"$((i+1))]} "USD";
 done
