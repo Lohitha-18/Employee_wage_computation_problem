9832e778619a9aefea5778dd26f85f4099ebb51b
lohitha <devarajlohitha@gmail.com>
2023-03-21 18:50:26 +0530

    Added:Refactor the code to write function to get workhours

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -8,18 +8,24 @@ hours_worked=0
 days_worked=0
 total_wages=0
 
+get_work_hours() {
+  read -p "Enter the number of work hours for day $1: " hours_per_day
+  echo $hours_per_day
+}
+
 while [[ $hours_worked -lt $TOTAL_WORKING_HOURS && $days_worked -lt $TOTAL_WORKING_DAYS ]]
 do
+  # Get work hours for current day
+  hours_per_day=$(get_work_hours $(($days_worked + 1)))
+
   # Calculate daily wage
   daily_wage=$(($WAGE_PER_HOUR * $hours_per_day))
 
-  # Add daily wage to total wages
   total_wages=$(($total_wages + $daily_wage))
 
-  # Increment hours and days worked
   hours_worked=$(($hours_worked + $hours_per_day))
   days_worked=$(($days_worked + 1))
 done
 
-# Output total wages earned
 echo "Total wages earned: $total_wages"
+
