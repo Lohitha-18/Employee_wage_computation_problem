c47e499458a15e8d5c49a0ad418b106d2c5b3776
lohitha <devarajlohitha@gmail.com>
2023-03-21 18:21:21 +0530

    Added:calculating employee wage using switch case statement

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -5,12 +5,14 @@ PRESENT=1;
 PART_TIME=2
 isPresent=$((RANDOM%3)); #0-Absent 1-Present 2-Part_time
 
-if [ $isPresent -eq $PRESENT ]
-then
-	echo "Employee is Present";
-elif [  $isPresent -eq $PART_TIME ]
-then
-	echo "Employee is working as part time";
-else
-	echo "Employee is Absent";
-fi
+case $isPresent in 
+	0)
+		echo "Employee is Absent";
+	;;
+	1)
+		echo "Employee is Present";
+	;;
+	2)
+		echo "Employee is working as part time";
+	;;
+esac
